09637e2b0c86e89df3bb38184819e1fd052ae01c
veewee <toon.verwerft@phpro.be>
2013-09-06 14:16:28 +0200

    Make the params service a factory in the service manager configuration.

--- config/module.config.php
+++ config/module.config.php
@@ -3,6 +3,7 @@ return array(
     'service_manager' => array(
         'factories' => array(
             'phpro.smartcrud'           => 'PhproSmartCrud\Service\CrudServiceFactory',
+            'phpro.smartcrud.params'    => 'PhproSmartCrud\Service\ParametersService',
         ),
         'invokables' => array(
             // Services
@@ -12,7 +13,6 @@ return array(
             'phpro.smartcrud.read'      => 'PhproSmartCrud\Service\ReadService',
             'phpro.smartcrud.update'    => 'PhproSmartCrud\Service\UpdateService',
             'phpro.smartcrud.delete'    => 'PhproSmartCrud\Service\DeleteService',
-            'phpro.smartcrud.params'    => 'PhproSmartCrud\Service\ParametersService',
 
             // Gateways
             'phpro.smartcrud.gateway.doctrine'  => 'PhproSmartCrud\Gateway\DoctrineCrudGateway',
