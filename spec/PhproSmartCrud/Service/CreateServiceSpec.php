c3f7e0bd60a8a58dc65b8e4e7c815bb6a09a9556
Toon Verwerft <toon.verwerft@phpro.be>
2014-01-06 17:29:11 +0100

    Fix the link to the github repository in the top-docblock.

--- spec/PhproSmartCrud/Service/CreateServiceSpec.php
+++ spec/PhproSmartCrud/Service/CreateServiceSpec.php
@@ -2,7 +2,7 @@
 /**
  * Smartcrud for Zend Framework (http://framework.zend.com/)
  *
- * @link http://github.com/veewee/PhproSmartCrud for the canonical source repository
+ * @link http://github.com/phpro/zf-smartcrud for the canonical source repository
  * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
  * @license http://framework.zend.com/license/new-bsd New BSD License
  */
