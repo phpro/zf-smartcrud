6027b6c5b12f3c841c8afb8001b28c9417b4f5a4
Stef Liekens <stef.liekens@phpro.be>
2015-03-18 08:13:12 +0100

    Added also formdata to the CrudEvents triggered before and after create/update

--- src/Phpro/SmartCrud/Service/UpdateService.php
+++ src/Phpro/SmartCrud/Service/UpdateService.php
@@ -36,9 +36,9 @@ class UpdateService extends AbstractSmartService
             $form->setData($data);
             $em->trigger($this->createEvent(CrudEvent::BEFORE_DATA_VALIDATION, $form, array('postData' => $data)));
             if ($form->isValid()) {
-                $em->trigger($this->createEvent(CrudEvent::BEFORE_UPDATE, $entity));
+                $em->trigger($this->createEvent(CrudEvent::BEFORE_UPDATE, $entity, $data));
                 $result->setSuccess($this->getGateway()->update($entity, $data));
-                $em->trigger($this->createEvent(CrudEvent::AFTER_UPDATE, $entity));
+                $em->trigger($this->createEvent(CrudEvent::AFTER_UPDATE, $entity, $data));
             } else {
                 $em->trigger($this->createEvent(CrudEvent::INVALID_UPDATE, $form));
             }
