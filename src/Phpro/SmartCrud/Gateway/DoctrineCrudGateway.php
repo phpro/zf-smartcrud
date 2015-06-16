e2ce504a49c3e3e8166e0e7653270afc8fcc6850
Stef Liekens <stef.liekens@phpro.be>
2015-06-16 21:05:18 +0200

    Fixed braces in DoctrineCrudGateway

--- src/Phpro/SmartCrud/Gateway/DoctrineCrudGateway.php
+++ src/Phpro/SmartCrud/Gateway/DoctrineCrudGateway.php
@@ -24,7 +24,6 @@ use Phpro\SmartCrud\Query\QueryProviderInterface;
 class DoctrineCrudGateway
     implements ObjectManagerAwareInterface, CrudGatewayInterface
 {
-
     const TYPE_ORM = 'ORM';
     const TYPE_ODM = 'ODM';
 
@@ -133,8 +132,8 @@ class DoctrineCrudGateway
     /**
      * @param $entity
      * @param $id
-     *
      * @return bool
+     * @throws \Exception
      */
     public function delete($entity, $id)
     {
